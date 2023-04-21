aa3b630e91ef94ff9388e2140b68d6e990eb3163
Eddy Ruiz <eddyvaldo@live.com.mx>
2023-04-21 16:04:18 +0000

    creando servidor

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+
+// importando el modulo http
+
+import http from 'http';
+
+
+//  crear el servidor
+
+const server = http.createServer((req, res) => {
+    // logica del server
+    // respondiendo 
+    res.write('Hello from the server');
+    // Cerrar la conexion 
+    res.end();
+});
+
+// arrancar el servidor
+
+server.listen(3000, "0.0.0.0", () => {
+    console.log('Servidor escuchando en HTTP://localhost:3000');
+});
